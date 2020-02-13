fd05bed6cf3351b124f32bb90f0f55fd5894ae6b
Johnson Kow <jkow95@gmail.com>
2020-02-13 18:37:44 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,22 @@
-# Add your code here
+require "pry" # Add your code here
+
+def help 
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end 
+
+def play(songs)
+  puts "Please enter a song name or number:"
+  input = gets.strip
+  binding.pry
+  songs.each do |song|
+    if input == song
+      puts "Playing #{song}"
+    else 
+      puts "Invalid input, please try again"
+    end 
+  end 
+end 
