8ee6d4dc5d45e871aa05ec278e6f9dbba2929957
Johnson Kow <jkow95@gmail.com>
2020-02-13 19:03:45 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,8 +11,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  song_num = []
-  for i < songs.length 
+  song_num = [1..9]
   
   songs.each do |song|
     if input == song || song_num 
