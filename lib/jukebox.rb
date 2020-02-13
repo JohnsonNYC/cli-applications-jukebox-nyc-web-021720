f75509a69ab3f79a924e3e69564ce7bb95a1bdd4
Johnson Kow <jkow95@gmail.com>
2020-02-13 18:50:39 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
   i = 0 
-  for i < songs.length do
+  for i > songs.length do
     song_number = songs[i] + 1
   end 
 end 
